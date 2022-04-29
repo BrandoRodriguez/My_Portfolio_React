c79505dbb0c020ac05527e79a2c9446cd50e514a
Brando Rodriguez <brandorodriguez@gmail.com>
2022-04-29 09:48:17 -0500

    subo cambios

--- client/src/Section/Portfolio.jsx
+++ client/src/Section/Portfolio.jsx
@@ -2,7 +2,7 @@ import './Portfolio.css'
 import Card from '../Components/Card.jsx'
 import Button from '../Components/Button.jsx'
 // import img_ecomerce from '../assets/images/img-projects/ecomerce.png'
-// import img_dogs from '../assets/images/img-projects/dogs.png'
+import img_dogs from '../assets/images/img-projects/dogs.webp'
 // import img_pokemon_mb from '../assets/images/img-projects/pokemon_mb.webp'
 import img_pokemon from '../assets/images/img-projects/pokemon.webp'
 
@@ -20,13 +20,13 @@ function Portfolio() {
                     title='Pokemon App, Full-Stack.' 
                     information='Project Objectives Build an App using React, Redux, Node and Sequelize.'
                     />
-                    {/* <Card number='02' 
-                    img={img_dogs} 
-                    badge='modern shop'
-                    title='JQ Official Store Website Design.'
-                    information='WooCommerce. Hub provides all the tools you need to start selling online. Let your customers to experience WooCommerce at its absolute-best.'
+                    <Card number='02' 
+                    img={img_dogs}
+                    badge='Pokemon App'
+                    title='Dog App, Full-Stack.' 
+                    information='Project Objectives Build an App using React, Redux, Node and Sequelize.'
                     />
-                    <Card number='03' 
+                    {/* <Card number='03' 
                     img={img_pokemon} 
                     badge='modern shop'
                     title='JQ Official Store Website Design.' 

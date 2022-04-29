a792414d45646e5cc58a2457e3d398e00c61ac88
Brando Rodriguez <brandorodriguez@gmail.com>
2022-04-29 11:52:31 -0500

    subo cambios

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -1,6 +1,7 @@
 import './Card.css'
 import Badge from './Badge.jsx'
 import Arrow from './Arrow.jsx'
+import Tec from './Tec.jsx';
 // import img_react from '../assets/images/img-tec/react.webp'
 // import img_node from '../assets/images/img-tec/node.webp'
 
@@ -22,12 +23,11 @@ function Card(props) {
                     <Badge badge={props.badge} />
                     <h2 className="text-white text-xl">{props.title}</h2>
                     <div className="cnt-icon-tec flex justify-center items-center">
-                        {/* <div className="icon-tec">
-                            <img src={img_react} alt="Logo_Tec - Brando Rodriguez - @brandorodriguez - brandorodriguez.com" loading="lazy" />
-                        </div>
-                        <div className="icon-tec">
-                            <img src={img_node} alt="Logo_Tec - Brando Rodriguez - @brandorodriguez - brandorodriguez.com" loading="lazy" />
-                        </div> */}
+                        <Tec tec='Stack PERN'/>
+                        <Tec tec='PostgreSQL'/>
+                        <Tec tec='Express'/>
+                        <Tec tec='React'/>
+                        <Tec tec='Node'/>
                     </div>
                     <p className="text-md">{props.information}</p>
                     <a href="/" className="text-white text-md flex gap items-center">
---
+++ client/src/Components/Tec.jsx
@@ -0,0 +1,11 @@
+import './Tec.css'
+
+function Tec(props) {
+    return (
+        <>
+            <span className='tec text-white'>{props.tec}</span>
+        </>
+    );
+}
+
+export default Tec;

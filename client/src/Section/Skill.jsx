86b995ca3ceb64df8c9cb0d3039a07fad96c0587
Brando Rodriguez <brandorodriguez@gmail.com>
2022-05-02 20:47:56 -0500

    subo cambios

--- client/src/Section/Skill.jsx
+++ client/src/Section/Skill.jsx
@@ -25,14 +25,24 @@ function Skill() {
                     description='Allows you to create dynamically updating content, control multimedia, etc.'
                     />     
                     <CardSkill
-                    abbreviation='CSS'
-                    name='Cascading Style Sheets.'
-                    description='Used to describe the presentation of HTML or XML documents.'    
+                    abbreviation='PostgreSQL'
+                    name='Database.'
+                    description='Object-oriented relational database management.'    
                     /> 
                     <CardSkill
-                    abbreviation='HTML'
-                    name='The Hypertext Markup Language.'
-                    description='Is used to structure and display a web page and its contents.'
+                    abbreviation='Express'
+                    name='Web application infrastructure.'
+                    description='Work environment for web applications for the Node.js programmer.'
+                    />   
+                    <CardSkill
+                    abbreviation='React'
+                    name='Open source javascript library.'
+                    description='Designed to create user interfaces with the aim of facilitating the development of applications on a single page.'
+                    />   
+                    <CardSkill
+                    abbreviation='Node'
+                    name='JavaScript runtime built on Chrome, V8 JavaScript engine.'
+                    description='Open source, cross-platform runtime environment for the server layer based on the JavaScript programming language.'
                     />                 
                 </div>
             </div>
